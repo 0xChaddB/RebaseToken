8d23b1dc1f1b935dfd495503b8d6ac3969b82580
0xChaddB <0xChaddB@proton.me>
2025-01-31 11:34:17 +0100

    feat: Vault contract creation

---
+++ src/Vault.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Vault {
+    // We need to pass the token address to the constructor.
+    // Create a deposit function that mints tokens to the user equal to the user
+    // Create a redeem function that bruns the tokens from the user and sends the user ETH.
+    // create a way to add rewards to the vault
+    address private immutable i_rebaseToken;
+
+    constructor(address _rebaseToken) {
+        i_rebaseToken = _rebaseToken;
+    }
+}
