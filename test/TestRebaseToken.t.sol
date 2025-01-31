605fc1aeb6fa875d58df3d61d1699ed78bb9ff64
0xChaddB <0xChaddB@proton.me>
2025-01-31 14:38:34 +0100

    test: testDepositLinear

--- test/TestRebaseToken.t.sol
+++ test/TestRebaseToken.t.sol
@@ -18,7 +18,37 @@ contract TestRebaseToken is Test {
         rebaseToken = new RebaseToken();
         vault = new Vault(IRebaseToken(address(rebaseToken)));
         rebaseToken.grantMintAndBurnRole(address(vault));
-        (bool success, ) = payable (address(vault)).call{value: 1 ether}("");
+        vm.stopPrank();
+    }
+    
+    function testDepositLinear(uint256 amount) public {
+        // vm.asume(amout > 1e4)
+        amount = bound(amount, 1e5, type(uint96).max);
+        // 1. Deposit
+        vm.startPrank(user);
+        vm.deal(user, amount);
+        vault.deposit{value: amount}();
+
+        // 2. Check our rebase token balance
+        uint256 startBalance = rebaseToken.balanceOf(user);
+        console.log("block.timestamp", block.timestamp);
+        console.log("start balance", startBalance);
+        assertEq(startBalance, amount);
+
+        // 3. Warp the time and check the balance again 
+        vm.warp(block.timestamp + 1 hours);
+        console.log("block.timestamp", block.timestamp);
+        uint256 middleBalance = rebaseToken.balanceOf(user);
+        console.log("middleBalance", middleBalance);
+        assertGt(middleBalance, startBalance);
+        // 4. Warp the time again by the same amount and check the balance again
+        vm.warp(block.timestamp + 1 hours);
+        console.log("block.timestamp", block.timestamp);
+        uint256 endBalance = rebaseToken.balanceOf(user);
+        console.log("endBalance", endBalance);
+        assertGt(endBalance, middleBalance);
+        
+        assertApproxEqAbs(endBalance - middleBalance, middleBalance - startBalance, 1);
         vm.stopPrank();
     }
 }
