a6eee64e8db6b9f15a51c2165d7a860c8c835143
0xChaddB <0xChaddB@proton.me>
2025-01-31 11:36:28 +0100

    feat: getReTokAddr func

--- src/Vault.sol
+++ src/Vault.sol
@@ -11,4 +11,8 @@ contract Vault {
     constructor(address _rebaseToken) {
         i_rebaseToken = _rebaseToken;
     }
+
+    function getRebaseTokenAddress() external view returns (address) {
+        return i_rebaseToken;
+    }
 }
