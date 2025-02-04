34e0e842e157ee1ddfcabd8f79140374b5f80cfb
0xChaddB <0xChaddB@proton.me>
2025-02-04 21:17:26 +0100

    feat: CCIP integration

--- src/Vault.sol
+++ src/Vault.sol
@@ -25,7 +25,8 @@ contract Vault {
     */
     function deposit() external payable {
         // 1. We need to use the amount of ETH the user has sent to visit tokens to the user 
-        i_rebaseToken.mint(msg.sender, msg.value);
+        uint256 interestRate = i_rebaseToken.getUserInterestRate();
+        i_rebaseToken.mint(msg.sender, msg.value, interestRate);
         emit Deposit(msg.sender, msg.value);
     }
 
